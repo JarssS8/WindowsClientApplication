09f2a926449639b1041b5f4e03793c6bb8ea601a
2dam <2dam@MACINF03.TartangaLH.eus>
2019-10-23 14:20:02 +0200

    windows client 23/10/19 version starting to build the clases more detailed

--- src/windowsclientapplication/controller/LogOutWindowController.java
+++ src/windowsclientapplication/controller/LogOutWindowController.java
@@ -5,10 +5,61 @@
  */
 package windowsclientapplication.controller;
 
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import utillities.beans.User;
+
 /**
  *
- * @author adria
+ * @author Diego
  */
 public class LogOutWindowController {
+    //TODO Logger need to be erased at the end!!
+    private static final Logger LOGGER = Logger
+            .getLogger("windowsclientapplication.LogOutWindowController");
+    
+    @FXML
+    private Menu menOpt;
+    @FXML
+    private Menu menHelp;
+    @FXML
+    private MenuItem menClose;
+    @FXML
+    private MenuItem menAbout;
+    
+    private User usr;
+    private Stage stage;
+    
+    
+    
+    
+    
+   private void handleButtonAction(ActionEvent event) {
+        label.setText(greeting);
+        
+        //TODO set te text to the labels
+        
+    }
+    
+    /**
+     * 
+     * @param e 
+     */
+    private void onWindowShowing(WindowEvent e){
+        label.setText(greeting);
+    }
+
+    /**
+     * 
+     * @param stage 
+     */
+    void setStage(Stage stage) {
+        this.stage=stage;
+    }
     
 }
