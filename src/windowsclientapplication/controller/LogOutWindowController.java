3f61df3a5694bb773bf8888a871ee5b425bf56a4
2dam <2dam@MACINF03.TartangaLH.eus>
2019-10-25 14:10:28 +0200

    Windows client application, LogOut window controller

--- src/windowsclientapplication/controller/LogOutWindowController.java
+++ src/windowsclientapplication/controller/LogOutWindowController.java
@@ -8,15 +8,18 @@ package windowsclientapplication.controller;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import utillities.beans.User;
 
 /**
  *
- * @author Diego
+ * @author Diego Urraca
  */
 public class LogOutWindowController {
     //TODO Logger need to be erased at the end!!
@@ -35,23 +38,46 @@ public class LogOutWindowController {
     private User usr;
     private Stage stage;
     
+    /**
+     * Method to initialize the window
+     * @param root 
+     */
+    public void initStage (Parent root){
+        try{
+            Scene scene = new Scene(root);
+            stage = new Stage();
+            //Window set to modal window
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(scene);
+            stage.setTitle("Welcome");
+            stage.setResizable(false);
+            stage.setOnShowing(this::onWindowShowing);
+            stage.showAndWait();
+        }
+        catch(Exception e){
+            //TODO have to code the exception for this initStage-Catch
+        }
+    }
     
     
     
-    
+    /**
+     * Method that handle the buttons press actions
+     * @param event 
+     */
    private void handleButtonAction(ActionEvent event) {
-        label.setText(greeting);
+        //label.setText(greeting);
         
-        //TODO set te text to the labels
+        //TODO set the text to the labels
         
     }
     
     /**
-     * 
+     * Method that loads the texts and prepare the objects of the window
      * @param e 
      */
     private void onWindowShowing(WindowEvent e){
-        label.setText(greeting);
+        LOGGER.info("Starting onWindoShowing");
     }
 
     /**
